208a34d296629e7b2a1de08f4f8df82770a4a739
sleepingkiwi <joe@sleeping.kiwi>
2022-05-19 13:15:02 +0100

    Make sure that JS which uses the dom only runs after DOM is ready!

--- src/js/entry.js
+++ src/js/entry.js
@@ -6,15 +6,23 @@ import { init as genericIntersectionInit } from './generic-intersection';
 import { init as genericActivatorsInit } from './generic-activation';
 import { init as carouselInit } from './carousels';
 
-
-/** Init everything
+/** Init everything that interacts with the DOM (after dom is ready)
  *  ------------------------------------------------------------------------------------------------
 **/
-// validation listeners for forms
-formsInit();
-// allowing elements to opt in to generic interesection observation by adding .js--wants-interestion
-genericIntersectionInit();
-// a generic set of classes for toggling, activating and deactivating things!
-genericActivatorsInit();
-// set up all the carousel listeners
-carouselInit();
+const init = () => {
+  // validation listeners for forms
+  formsInit();
+  // allowing elements to opt in to generic interesection observation
+  // by adding .js--wants-interestion
+  genericIntersectionInit();
+  // a generic set of classes for toggling, activating and deactivating things!
+  genericActivatorsInit();
+  // set up all the carousel listeners
+  carouselInit();
+};
+
+if (document.readyState === 'loading') { // Loading hasn't finished yet
+  document.addEventListener('DOMContentLoaded', init);
+} else { // `DOMContentLoaded` has already fired
+  init();
+}
